dc6c6eef4ebc2e9576390a1c223c799044f6695d
Mayra Alvarez <mayra.alvarez.velasquez@gmail.com>
2016-09-16 12:25:05 -0500

    agregando archivos

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function validateForm(){
+    
+	else if(email == ""){
+           cuadroNegro.appendChild(cuadroDeValidación);
+           cuadroDeValidación.innerHTML= "Verifique su E-mail";
+           cuadroNegro.classList.toggle("error");
+       }
+var cuadroNegro = document.getElementsByClassName("input-box")[2];
+}
