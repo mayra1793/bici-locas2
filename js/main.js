fe2c8c1b3b33d5334dd4652bffd3690ec477b682
Mayra Alvarez <mayra.alvarez.velasquez@gmail.com>
2016-09-16 14:16:49 -0500

    agregando main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 function validateForm(){
-    
-	else if(email == ""){
-           cuadroNegro.appendChild(cuadroDeValidación);
-           cuadroDeValidación.innerHTML= "Verifique su E-mail";
-           cuadroNegro.classList.toggle("error");
-       }
-var cuadroNegro = document.getElementsByClassName("input-box")[2];
+    var nombre = document.getElementById("name").value;
+        if(nombre == ""){
+            var hermano = document.getElementById("name");
+            var texto = document.createTextNode("Debe ingresar su nombre");
+            mensaje(hermano,texto);
+        }
+    var apellido = document.getElementById("lastname").value;
+       if(apellido == "") {
+          var hermano = document.getElementById("lastname");
+          var texto = document.createTextNode("Debe ingresar su apellido");
+          mensaje(hermano,texto);
+        }
+    var email = document.getElementById("input-email").value;
+    var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ( !expr.test(email) ){
+        var hermano = document.getElementById("input-email");
+        var texto = document.createTextNode("Verifique su email");
+        mensaje(hermano, texto);
+    }
+    var contra = document.getElementById("input-password").value;
+        if(contra.length < 5 ){
+            var hermano = document.getElementById("input-password");
+            var texto = document.createTextNode("La contraseña debe tener al menos 6 caracteres");
+            mensaje(hermano, texto);
+        }else if(contra == "123456" || contra =="password" || contra == "098754"){
+            alert("Por favor escriba otra contraseña");
+        }
+    if (document.getElementsByTagName("select")[0].value == 0){
+        var hermano = document.getElementsByTagName("select")[0];
+        var texto = document.createTextNode("Debes seleccionar al menos un tipo de bici");
+        mensaje(hermano, texto);
+
+    }
+}
+function mensaje (hermano, texto){
+    var mensajeNegro = document.createElement("span");
+    var padreMensaje = hermano.parentNode;
+    padreMensaje.insertBefore(mensajeNegro,hermano);
+    mensajeNegro.appendChild(texto);
+    return mensajeNegro;
 }
